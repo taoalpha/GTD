58c29a354433e372444a73776a93b5afec8088b9
taoalpha <iamzhoutao92@gmail.com>
2017-10-25 20:52:07 -0700

    set up server

---
+++ server/build/server.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+    res.send("Hello World!!!");
+});
+app.post("/", function (req, res) {
+    console.log(req.body);
+});
+app.listen(2000);
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+declare const require;
+const app = require('express')();
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+ 
+app.listen(2000);
